b118f4a893f7d47fd6e0499456ec2578298b4a41
Beatriz Castro <mbeatrizhcastro@gmail.com>
2025-10-11 11:48:19 +0200

    feat: add and fix bug

---
+++ exercises/bug.js
@@ -0,0 +1,17 @@
+// Sustituimos en el for <= por <. Con < el bucle va 0 hasta length - 1
+
+const calcularPromedio = (numeros) => {
+  let sumaTotal = 0;
+
+  for (let i = 0; i < numeros.length; i++) {
+    sumaTotal += numeros[i];
+  }
+
+  const promedio = sumaTotal / numeros.length;
+  return promedio;
+};
+
+const listaNumeros = [1, 2, 3, 4, 5];
+const promedioNumeros = calcularPromedio(listaNumeros);
+
+console.log(promedioNumeros) // 3
