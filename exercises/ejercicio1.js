f8e821dec277a4114b0fb6abbdce6bbbb8e55a1d
Beatriz Castro <mbeatrizhcastro@gmail.com>
2025-10-11 11:32:59 +0200

    feat: add ejercicio1

---
+++ exercises/ejercicio1.js
@@ -0,0 +1,15 @@
+console.log("BEA")
+
+
+const user = {
+  firstName: 'Beatriz',
+  lastName: 'H Castro',
+  courses: [
+    { name: 'Node.js', startDate: '2025-10-20' },
+    { name: 'Git', startDate: '2025-09-21' },
+    { name: 'React', startDate: '2025-12-09' }
+  ],
+  activeSearch: true
+};
+
+console.log(user.courses[2].startDate)
