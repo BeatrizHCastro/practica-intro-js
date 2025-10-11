011752c4da1fa45b6dbb2695698ca0fae08735c5
Beatriz Castro <mbeatrizhcastro@gmail.com>
2025-10-11 12:24:28 +0200

    feat: add and fix ejercicio3-2

--- exercises/ejercicio3.js
+++ exercises/ejercicio3.js
@@ -1,10 +1,10 @@
-// Inputs exercise
+// 3.1 Inputs exercise
 
 const input1 = ['Downloads', 'Videos', 'capture', 'mp4'];
 const input2 = ['CodinGame', 'python', 'py'];
 const input3 = ['programming', 'languages', 'easy', 'beginner', 'useful', 'pythonstuff', 'py'];
 
-// Create here your function
+// 3.1 Create here your function
 
 const buildFilePath = (segments) => {
 
@@ -25,6 +25,33 @@ buildFilePath(input1); // 'Downloads/Videos/capture.mp4'
 buildFilePath(input2); // 'CodinGame/python.py'
 buildFilePath(input3); // 'programming/languages/easy/beginner/useful/pythonstuff.py'
 
-// console.log(buildFilePath(input1))
-// console.log(buildFilePath(input2))
 // console.log(buildFilePath(input3))
+
+
+
+// 3.2 Inputs exercise
+
+const numberInput1 = 10;
+const numberInput2 = 1;
+const numberInput3 = 11234;
+
+// 3.2 Create here your function
+
+const formatNumberWithDashes = (numberValue) => {
+  
+  const textDigits = '' + numberValue;
+
+  const digitsArray = textDigits.split('');
+
+  const digitsCopy = structuredClone(digitsArray);
+
+  const dashedText = digitsCopy.join('-');
+
+  return dashedText;
+};
+
+formatNumberWithDashes(numberInput1); // 1-0
+formatNumberWithDashes(numberInput2); // 1
+formatNumberWithDashes(numberInput3); // 1-1-2-3-4
+
+// console.log(formatNumberWithDashes(numberInput3))
